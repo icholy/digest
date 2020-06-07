daa77542b4f23aa4f2a4efc272352c86d765633b
Ilia Choly <ilia.choly@gmail.com>
2020-06-07 13:06:14 -0400

    use example from rfc

--- digest_test.go
+++ digest_test.go
@@ -6,33 +6,33 @@ import (
 	"gotest.tools/v3/assert"
 )
 
-// I pulled this test from: https://github.com/bobziuchkovski/digest
+// https://tools.ietf.org/html/rfc7616#section-3.9
 func TestDigest(t *testing.T) {
 	opt := Options{
 		Method:   "GET",
 		URI:      "/dir/index.html",
 		Username: "Mufasa",
-		Password: "Circle Of Life",
-		Cnonce:   "0a4f113b",
+		Password: "Circle of Life",
+		Cnonce:   "f2/wE4q74E6zIJEtWaHKaf5wv/H5QzzpXusqGemxURZJ",
 	}
 	chal := &Challenge{
-		Realm:     "testrealm@host.com",
-		Nonce:     "dcd98b7102dd2f0e8b11d0f600bfb0c093",
+		Realm:     "http-auth@example.org",
+		Nonce:     "7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v",
 		Algorithm: "MD5",
-		Opaque:    "5ccc069c403ebaf9f0171e9517f40e41",
+		Opaque:    "FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS",
 		QOP:       []string{"auth", "auth-int"},
 	}
 	cred, err := Digest(chal, opt)
 	assert.NilError(t, err)
 	assert.DeepEqual(t, cred, &Credentials{
 		Username:  "Mufasa",
-		Realm:     "testrealm@host.com",
-		Nonce:     "dcd98b7102dd2f0e8b11d0f600bfb0c093",
+		Realm:     "http-auth@example.org",
+		Nonce:     "7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v",
 		URI:       "/dir/index.html",
-		Response:  "6629fae49393a05397450978507c4ef1",
+		Response:  "8ca523f5e9506fed4657c9700eebdbec",
 		Algorithm: "MD5",
-		Cnonce:    "0a4f113b",
-		Opaque:    "5ccc069c403ebaf9f0171e9517f40e41",
+		Cnonce:    "f2/wE4q74E6zIJEtWaHKaf5wv/H5QzzpXusqGemxURZJ",
+		Opaque:    "FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS",
 		QOP:       "auth",
 		Nc:        1,
 	})
